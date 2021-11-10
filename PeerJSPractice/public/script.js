e87a31495b7ef61388c1914dc5717745edf7f38f
Diane Kim <dhk5118@psu.edu>
2021-11-09 20:37:12 -0500

    making comments

--- PeerJSPractice/public/script.js
+++ PeerJSPractice/public/script.js
@@ -1,47 +1,53 @@
-const socket = io('/')
-const videoGrid = document.getElementById('video-grid')
-const myPeer = new Peer(undefined, {
-  host: '/',
-  port: '3001'
+const socket = io('/') //making socket
+const videoGrid = document.getElementById('video-grid') //finding the Video-Grid element
+const myPeer = new Peer(undefined, { //making the peer element which is the user
+  host: 'localhost',
+  port: '3001',
+  secure: false
 })
 const myVideo = document.createElement('video')
 myVideo.muted = true
 const peers = {}
+
 navigator.mediaDevices.getUserMedia({
   video: true,
   audio: true
 }).then(stream => {
-  addVideoStream(myVideo, stream)
+  addVideoStream(myVideo, stream) //showing our video to ourselves
 
-  myPeer.on('call', call => {
-    call.answer(stream)
-    const video = document.createElement('video')
-    call.on('stream', userVideoStream => {
-      addVideoStream(video, userVideoStream)
+
+  myPeer.on('call', call => { //when we join the room, get a "call"
+    call.answer(stream) //stream our video/audio
+    const video = document.createElement('video') //create a video tag
+    call.on('stream', userVideoStream => { //receiving their video stream
+      addVideoStream(video, userVideoStream) //displaying their video 
     })
   })
 
-  socket.on('user-connected', userId => {
+  socket.on('user-connected', userId => { //when new user connects
     const fc = () => connectToNewUser(userId, stream)
     timerid = setTimeout(fc, 1000 )
+    
   })
 })
 
-socket.on('user-disconnected', userId => {
+
+
+socket.on('user-disconnected', userId => { //when user disconnects or closes window
   if (peers[userId]) peers[userId].close()
 })
 
-myPeer.on('open', id => {
+myPeer.on('open', id => { //when first open, you can join a room
   socket.emit('join-room', ROOM_ID, id)
 })
 
-function connectToNewUser(userId, stream) {
-  const call = myPeer.call(userId, stream)
-  const video = document.createElement('video')
+function connectToNewUser(userId, stream) { //when someone joins our room
+  const call = myPeer.call(userId, stream) //calling person that joined
+  const video = document.createElement('video') //adding the person's video
   call.on('stream', userVideoStream => {
     addVideoStream(video, userVideoStream)
   })
-  call.on('close', () => {
+  call.on('close', () => {//once they leave, remove their video display
     video.remove()
   })
 
@@ -50,8 +56,9 @@ function connectToNewUser(userId, stream) {
 
 function addVideoStream(video, stream) {
   video.srcObject = stream
-  video.addEventListener('loadedmetadata', () => {
+  video.addEventListener('loadedmetadata', () => {//stream video while it's being loaded
     video.play()
   })
-  videoGrid.append(video)
+  videoGrid.append(video) //add video to the videoGrid
 }
+
--- PeerJSPractice/server.js
+++ PeerJSPractice/server.js
@@ -1,24 +1,29 @@
-const express = require('express')
-const app = express()
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
-const { v4: uuidV4 } = require('uuid')
+//making express and socket.io servers
+const express = require('express') //express server
+const app = express() //app variable = to running express function
+const server = require('http').Server(app) //server is being made to use socket.io --passes in app object
+const io = require('socket.io')(server) //pass in the server to return of the require function
+const { v4: uuidV4 } = require('uuid') 
+const cors = require('cors')
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.set('view engine', 'ejs') // how were going to render our views (used ejs library)
+app.use(express.static('public')) // put all js and css in public folder, express.static is rendered through script
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.redirect(`/${uuidV4()}`)
+app.get('/', (req, res) => { //takes in request and response create a brand new room and redirect user to that room
+  res.redirect(`/${uuidV4()}`) // want a dynamic room (uses the uuid)
 })
 
-app.get('/:room', (req, res) => {
-  res.render('room', { roomId: req.params.room })
+//if join a specific room, it'll be rendered
+app.get('/:room', (req, res) => { //route for the room, dynamic parameter
+  res.render('room', { roomId: req.params.room }) 
 })
 
-io.on('connection', socket => {
+io.on('connection', socket => { //when someone connects to a room
   socket.on('join-room', (roomId, userId) => {
     socket.join(roomId)
     socket.broadcast.to(roomId).emit('user-connected', userId)
+    console.log(userId)
 
     socket.on('disconnect', () => {
         socket.broadcast.to(roomId).emit('user-disconnected', userId)
@@ -26,4 +31,7 @@ io.on('connection', socket => {
   })
 })
 
-server.listen(3000)
+
+server.listen(3000) //sets up server on port 3000
+//will be Cannot GET / when first starting up
+
