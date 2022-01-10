60931f86fb3e6e5421e2787341b11059c4e3f8e5
Diane Kim <dhk5118@psu.edu>
2022-01-10 16:43:30 -0500

    making changes

--- PeerJSPractice/public/script.js
+++ PeerJSPractice/public/script.js
@@ -1,20 +1,20 @@
 const socket = io('/'); //making socket
 const videoGrid = document.getElementById('video-grid'); //finding the Video-Grid element
-const myPeer = new Peer(undefined, { //making the peer element which is the user
-    host: 'localhost',
-    port: '3001',
-    secure: false
-});
 const myVideo = document.createElement('video');
 myVideo.muted = true;
-const peers = {};
 
+var myPeer = new Peer(undefined, { //making the peer element which is the user
+    host: '/',
+    port: '3001' 
+});
+let myVideoStream;
+const peers = {};
 navigator.mediaDevices.getUserMedia({
     video: true,
     audio: true
 }).then(stream => {
+    myVideoStream = stream;
     addVideoStream(myVideo, stream); //showing our video to ourselves
-
 
     myPeer.on('call', call => { //when we join the room, get a "call"
         call.answer(stream) //stream our video/audio
@@ -24,40 +24,36 @@ navigator.mediaDevices.getUserMedia({
         });
     });
 
-    socket.on('user-connected', userId => { //when new user connects
-        const fc = () => connectToNewUser(userId, stream);
-        timerid = setTimeout(fc, 1000);
-
+    socket.on('user-connected', (userId) => { //when new user connects
+        connectToNewUser(userId, stream);
     });
 });
 
-
-
 socket.on('user-disconnected', userId => { //when user disconnects or closes window
-    if (peers[userId]) peers[userId].close()
+    if (peers[userId]) peers[userId].close();
 });
 
 myPeer.on('open', id => { //when first open, you can join a room
-    socket.emit('join-room', ROOM_ID, id)
+    socket.emit('join-room', ROOM_ID, id);
 });
 
 function connectToNewUser(userId, stream) { //when someone joins our room
-    const call = myPeer.call(userId, stream) //calling person that joined
-    const video = document.createElement('video') //adding the person's video
-    call.on('stream', userVideoStream => {
-        addVideoStream(video, userVideoStream)
+    const call = myPeer.call(userId, stream); //calling person that joined
+    const video = document.createElement('video'); //adding the person's video
+    call.on('stream', (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
     });
     call.on('close', () => { //once they leave, remove their video display
-        video.remove()
+        video.remove();
     });
 
-    peers[userId] = call
+    peers[userId] = call;
 };
 
 function addVideoStream(video, stream) {
-    video.srcObject = stream
+    video.srcObject = stream;
     video.addEventListener('loadedmetadata', () => { //stream video while it's being loaded
-        video.play()
+        video.play();
+        videoGrid.append(video); //add video to the videoGrid
     });
-    videoGrid.append(video) //add video to the videoGrid
 };
--- PeerJSPractice/server.js
+++ PeerJSPractice/server.js
@@ -1,36 +1,72 @@
+require("dotenv").config();
 //making express and socket.io servers
 const express = require('express'); //express server
 const app = express(); //app variable = to running express function
-const server = require('http').Server(app); //server is being made to use socket.io --passes in app object
-const io = require('socket.io')(server); //pass in the server to return of the require function
+const http = require('http').Server(app); //server is being made to use socket.io --passes in app object
+const https = require('https').Server(sslOptions, app); //USE AND DIFFERENTIATE HTTP and HTTPS
+const path = require("path");
+var PORT = process.env.PORT || 3000;
 const { v4: uuidV4 } = require('uuid');
-const cors = require('cors');
-
 app.set('view engine', 'ejs'); // how were going to render our views (used ejs library)
+const io = require('socket.io')(http);
+var fs = require('fs');
+//express config when client connects to server at 443
+var PeerServer = require('peer').PeerServer ({
+    port: 443,
+    proxied: true,
+    ssl: {key: fs.readFileSync('/Users/diane/server.key'),
+    certificate: fs.readFileSync('/Users/diane/server.crt')}
+});
+const { ExpressPeerServer } = require('peer');
+// const expressPeerServer = ExpressPeerServer(http, {
+//     debug: true
+// });
+
+var sslOptions = { //accessing ssl key and certificate
+    key: fs.readFileSync('/Users/diane/server.key'),
+    cert: fs.readFileSync('/Users/diane/server.crt')
+  };
+
+var server = http.listen(PORT);
+https.listen(443);
+
+// CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+// app.use('/peerjs', expressPeerServer);
+app.use('/peer', ExpressPeerServer(server, {debug:true}));
+app.use('/peerjs', PeerServer);
 app.use(express.static('public')); // put all js and css in public folder, express.static is rendered through script
-app.use(cors());
 
 app.get('/', (req, res) => { //takes in request and response create a brand new room and redirect user to that room
-    res.redirect(`/${uuidV4()}`) // want a dynamic room (uses the uuid)
+    res.redirect(`/${uuidV4()}`); // want a dynamic room (uses the uuid)
 });
 
 //if join a specific room, it'll be rendered
 app.get('/:room', (req, res) => { //route for the room, dynamic parameter
-    res.render('room', { roomId: req.params.room })
+    res.render('room', { roomId: req.params.room });
 });
 
 io.on('connection', socket => { //when someone connects to a room
     socket.on('join-room', (roomId, userId) => {
-        socket.join(roomId)
-        socket.broadcast.to(roomId).emit('user-connected', userId)
-        console.log(userId)
+        socket.join(roomId);
+        socket.broadcast.to(roomId).emit('user-connected', userId);
+        console.log(userId);
 
         socket.on('disconnect', () => {
-            socket.broadcast.to(roomId).emit('user-disconnected', userId)
-        })
+            socket.broadcast.to(roomId).emit('user-disconnected', userId);
+        });
     });
 });
 
+// if (process.env.PROD){
+//     app.use(express.static(path.join(__dirname, './server.js')));
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.join(__dirname, './room.ejs'));
+//     });
+// }
 
-server.listen(3000); //sets up server on port 3000
-//will be Cannot GET / when first starting up
